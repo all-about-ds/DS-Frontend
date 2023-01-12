ba77806e84e4f2dacc5efe3ac35e0ac66283cf49
ParkSeoJune <tjwns4122@gmail.com>
2023-01-12 17:53:10 +0900

    interceptor설정

--- src/utils/TokenService.ts
+++ src/utils/TokenService.ts
@@ -1,27 +1,27 @@
-import { TokenInterface } from "types/token";
+import { TokenInterface } from 'types/token';
 
 class TokenService {
   getLocalRefreshToken() {
-    const user = JSON.parse(localStorage.getItem("token") || "{}");
+    const user = JSON.parse(localStorage.getItem('token') || '{}');
     return user?.refreshToken;
   }
   getLocalAccessToken() {
-    const user = JSON.parse(localStorage.getItem("token") || "{}");
+    const user = JSON.parse(localStorage.getItem('token') || '{}');
     return user?.accessToken;
   }
   updateLocalAccessToken(token: string) {
-    let user = JSON.parse(localStorage.getItem("token") || "{}");
+    const user = JSON.parse(localStorage.getItem('token') || '{}');
     user.accessToken = token;
-    localStorage.setItem("token", JSON.stringify(user));
+    localStorage.setItem('token', JSON.stringify(user));
   }
   getUser() {
-    return JSON.parse(localStorage.getItem("token") || "{}");
+    return JSON.parse(localStorage.getItem('token') || '{}');
   }
   setUser(user: TokenInterface) {
-    localStorage.setItem("token", JSON.stringify(user));
+    localStorage.setItem('token', JSON.stringify(user));
   }
   removeUser() {
-    localStorage.removeItem("token");
+    localStorage.removeItem('token');
   }
 }
 
