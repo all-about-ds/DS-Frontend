0e0d0b4d2525d052673d8ddf63e1a8541c8265f0
jongjin <jongjin024@gmail.com>
2023-01-26 18:11:24 +0900

    add: GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import SignupPage from 'components/signup';
+import GlobalStyle from 'shared/style';
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <GlobalStyle />
+      <SignupPage />
+    </>
+  );
 }
 
 export default App;
