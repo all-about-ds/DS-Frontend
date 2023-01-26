775e687982b4341e95473352a5cd67ff0c50f1e5
jongjin <jongjin024@gmail.com>
2023-01-26 19:02:08 +0900

    add: progress bar props

--- src/components/common/layout/auth/index.tsx
+++ src/components/common/layout/auth/index.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import * as S from './style';
 
-interface AuthLayoutProps extends React.HTMLAttributes<HTMLButtonElement> {
+interface AuthLayoutProps extends React.HTMLAttributes<HTMLElement> {
   children: React.ReactNode;
-  title: string;
+  title: '회원가입' | '로그인' | '비밀번호 찾기';
+  progressBar: {
+    need: boolean;
+    value: number;
+  };
 }
 
 function AuthLayout(props: AuthLayoutProps) {
   return (
-    <>
-      <S.SignFrame>
-        <S.TitleText>{props.title}</S.TitleText>
-        {props.children}
-      </S.SignFrame>
-    </>
+    <S.AuthFrame>
+      <S.TitleText>{props.title}</S.TitleText>
+      {props.progressBar.need ? (
+        <S.ProgressBar
+          id='progress'
+          value={props.progressBar.value}
+          max='100'
+        />
+      ) : (
+        <></>
+      )}
+      {props.children}
+    </S.AuthFrame>
   );
 }
 
--- src/components/common/layout/auth/style.ts
+++ src/components/common/layout/auth/style.ts
@@ -1,11 +1,10 @@
 import styled from 'styled-components';
 
-export const SignFrame = styled.div`
+export const AuthFrame = styled.div`
   width: 21vw;
   min-width: 372px;
   height: 55vh;
   min-height: 600px;
-
   background: #161616;
   border-radius: 10px;
   display: flex;
@@ -18,4 +17,20 @@ export const TitleText = styled.div`
   font-size: 24px;
   color: #ffffff;
   margin-top: 25px;
+`;
+
+export const ProgressBar = styled.progress`
+  width: 100%;
+  height: 2px;
+  appearance: none;
+
+  ::-webkit-progress-bar {
+    background: #333333;
+  }
+
+  ::-webkit-progress-value {
+    background: #7139ea;
+    transition: width 0.5s;
+  }
+  margin: 25px 0 38px 0;
 `;
