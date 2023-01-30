7e17a3015eed44039048371f34264bf8dda924f4
jongjin <jongjin024@gmail.com>
2023-01-30 15:17:24 +0900

    add: recoil root

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import GlobalStyle from 'shared/style';
 import SigninPage from 'pages/auth/signinPage';
 import { Route, Routes } from 'react-router-dom';
 import SignupPage from 'components/signup/page';
+import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
-    <div>
+    <RecoilRoot>
       <GlobalStyle />
       <Routes>
         <Route path='/signin' element={<SigninPage />} />
         <Route path='/signup' element={<SignupPage />} />
       </Routes>
-    </div>
+    </RecoilRoot>
   );
 }
 
