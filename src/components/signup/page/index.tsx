4958ae64dcd20e6deb02cda8b8785306fc23cf50
jongjin <jongjin024@gmail.com>
2023-01-30 15:20:33 +0900

    add: signup first section ui

--- src/components/signup/page/index.tsx
+++ src/components/signup/page/index.tsx
@@ -1,20 +1,32 @@
+import {
+  signupCurrentSectionAtom,
+  signupEmailDuplicationModalAtom,
+} from 'atoms';
 import Background from 'components/common/background';
 import AuthLayout from 'components/common/layout/auth';
-import SignupFirstSection from '../section/first';
+import SignupEmailDuplicationModal from 'components/modals/signupException';
+import { useRecoilState } from 'recoil';
+import SignupSectionContainer from '../container';
 import * as S from './style';
 
 function SignupPage() {
+  const [signupEmailDuplicationModal, _] = useRecoilState(
+    signupEmailDuplicationModalAtom
+  );
+
   return (
-    <Background>
-      <AuthLayout title='회원가입' progressBar={{ need: true, value: 33 }}>
-        <SignupFirstSection />
-        <S.SignupButton>다음</S.SignupButton>
-        <S.GoLoginBox>
-          <p>기존 회원이신가요?</p>
-          <p style={{ color: '#7139EA', cursor: 'pointer' }}>로그인</p>
-        </S.GoLoginBox>
-      </AuthLayout>
-    </Background>
+    <>
+      {signupEmailDuplicationModal && <SignupEmailDuplicationModal />}
+      <Background>
+        <AuthLayout title='회원가입' progressBar={{ need: true, value: 33 }}>
+          <SignupSectionContainer />
+          <S.GoLoginBox>
+            <p>기존 회원이신가요?</p>
+            <p style={{ color: '#7139EA', cursor: 'pointer' }}>로그인</p>
+          </S.GoLoginBox>
+        </AuthLayout>
+      </Background>
+    </>
   );
 }
 
--- src/components/signup/page/style.ts
+++ src/components/signup/page/style.ts
@@ -9,31 +9,6 @@ export const Title = styled.p`
   margin-bottom: 25px;
 `;
 
-export const SignupButton = styled.button`
-  width: 16.5vw;
-  min-width: 284px;
-  margin: 11rem auto 0;
-  outline: none;
-  display: block;
-  border: none;
-  cursor: pointer;
-  height: 40px;
-  background: #7848de;
-  border-radius: 8px;
-  font-style: normal;
-  font-weight: 400;
-  font-size: 14px;
-  color: #ffffff;
-  transition: 0.2s;
-
-  :hover {
-    box-sizing: border-box;
-    border: 1px solid #7848de;
-    color: #7848de;
-    background: #161616;
-  }
-`;
-
 export const GoLoginBox = styled.div`
   display: flex;
   justify-content: center;
