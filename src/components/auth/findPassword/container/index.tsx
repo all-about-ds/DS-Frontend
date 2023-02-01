cc6ae2a3f6c4c30292763a99f2458e5ba13cad74
jongjin <jongjin024@gmail.com>
2023-02-01 18:51:05 +0900

    fix: section rendering logic

--- src/components/auth/findPassword/container/index.tsx
+++ src/components/auth/findPassword/container/index.tsx
@@ -1,24 +1,25 @@
-import { signupCurrentSectionAtom } from 'atoms';
+import { findPasswordCurrentSectionAtom } from 'atoms';
 import { useRecoilState } from 'recoil';
 import FindPasswordFirstSection from './sections/first';
-import SignupSecondSection from './sections/second';
-import SignupThirdSection from './sections/third';
+import FindPasswordSecondSection from './sections/second';
+import FindPasswordThirdSection from './sections/third';
 
 function FindPasswordSectionContainer() {
-  const [signupCurrentSection, _] = useRecoilState(signupCurrentSectionAtom);
-
+  const [findPasswordCurrentSection, _] = useRecoilState(
+    findPasswordCurrentSectionAtom
+  );
   const sectionRendering = () => {
     const view: any[] = [];
 
-    switch (signupCurrentSection) {
+    switch (findPasswordCurrentSection) {
       case 1:
         view.push(<FindPasswordFirstSection key={1} />);
         break;
       case 2:
-        view.push(<SignupSecondSection key={2} />);
+        view.push(<FindPasswordSecondSection key={2} />);
         break;
       case 3:
-        view.push(<SignupThirdSection key={3} />);
+        view.push(<FindPasswordThirdSection key={3} />);
         break;
     }
 
