fe14940408b713ff07a9dc1699e3a4d6de7157d1
jongjin <jongjin024@gmail.com>
2023-02-01 16:05:11 +0900

    fix: align center layout style

--- src/components/common/layout/align/center/style.ts
+++ src/components/common/layout/align/center/style.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const CenterAlignmentLayout = styled.div`
   width: 100vw;
-  height: 100vh;
+  height: calc(100vh - 81px);
   display: flex;
   flex-direction: column;
   align-items: center;
