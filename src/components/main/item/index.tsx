9465c319d6987928c2905b81ebcc543d5dfbd3ab
ParkSeoJune <tjwns4122@gmail.com>
2023-03-02 21:03:38 +0900

    fix: folder structure

---
+++ src/components/main/item/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import * as S from './style';
+
+function Group() {
+  return (
+    <>
+      <S.GroupBox>
+        <S.Image />
+        <S.MaxPeople>정원 20명</S.MaxPeople>
+        <S.Title>광주소프트웨어마이스터고등학교</S.Title>
+        <S.Description>
+          광주광역시 광산구 송정동에 위치한 SW 마이스터 고등학교로 대한민국에서
+          세 번째로 개교한 마이스터고이고 가나다라마바사
+        </S.Description>
+        <S.User>
+          <S.Profile image=''></S.Profile>
+          <S.UserName>오종진</S.UserName>
+        </S.User>
+      </S.GroupBox>
+    </>
+  );
+}
+
+export default Group;
--- src/components/main/page/index.tsx
+++ src/components/main/page/index.tsx
@@ -2,6 +2,7 @@ import Header from 'components/common/header';
 import CenterAlignmentLayout from 'components/common/layout/align/center';
 import MainFrame from 'components/frame/main';
 import React, { useState } from 'react';
+import Group from '../item';
 import * as S from './style';
 
 function Main() {
@@ -42,19 +43,7 @@ function Main() {
             </S.SortButton>
           </S.SortButtonWrapper>
           <S.GroupBoxWrapper>
-            <S.GroupBox>
-              <S.Image />
-              <S.MaxPeople>정원 20명</S.MaxPeople>
-              <S.Title>광주소프트웨어마이스터고등학교</S.Title>
-              <S.Description>
-                광주광역시 광산구 송정동에 위치한 SW 마이스터 고등학교로
-                대한민국에서 세 번째로 개교한 마이스터고이고 가나다라마바사
-              </S.Description>
-              <S.User>
-                <S.Profile image=''></S.Profile>
-                <S.UserName>오종진</S.UserName>
-              </S.User>
-            </S.GroupBox>
+            <Group />
           </S.GroupBoxWrapper>
         </MainFrame>
       </CenterAlignmentLayout>
