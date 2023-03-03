6725c885b4710fb9e8d4bd6f0b59b629aab80bc9
jongjin <jongjin024@gmail.com>
2023-03-03 15:06:49 +0900

    add: MyPage UI

--- src/components/my/index.tsx
+++ src/components/my/index.tsx
@@ -1,5 +1,46 @@
+import Header from 'components/common/header';
+import * as S from './style';
+import * as I from 'assets/svg';
+
 function My() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <S.MyPageLayout>
+        <S.ProfileSection>
+          <S.NameBox>
+            <S.Name>오종진님의 프로필</S.Name>
+            <I.NormalIcon />
+          </S.NameBox>
+          <S.Description>
+            프로필 사진과 닉네임으로 자신을 표현해봐요.
+          </S.Description>
+          <S.ProfileBox>
+            <S.ProfileImage src='https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201610/20/htm_2016102016349443859.jpg' />
+            <div>
+              <S.UpdateBox>
+                <I.UpdateProfileImageIcon />
+                <I.UpdateNameIcon />
+                <p>오종진</p>
+              </S.UpdateBox>
+              <S.LogoutButton>로그아웃</S.LogoutButton>
+            </div>
+          </S.ProfileBox>
+        </S.ProfileSection>
+        <S.GroupSection>
+          <S.GroupText>내 그룹</S.GroupText>
+          <S.GroupList>
+            <S.GroupItem>
+              <S.ItemBackgroundImage src='https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201610/20/htm_2016102016349443859.jpg' />
+              <S.Shadow>
+                <S.ItemName>GSM 공부방</S.ItemName>
+              </S.Shadow>
+            </S.GroupItem>
+          </S.GroupList>
+        </S.GroupSection>
+      </S.MyPageLayout>
+    </>
+  );
 }
 
 export default My;
