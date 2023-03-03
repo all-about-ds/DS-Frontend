56f2cb22485487d5f9a49f8dea741444003ca242
jongjin <jongjin024@gmail.com>
2023-03-03 15:06:42 +0900

    add: MyPage icons

--- src/assets/svg/index.tsx
+++ src/assets/svg/index.tsx
@@ -3,3 +3,9 @@ export { Search, GraySearch } from './search';
 export { MakeGroup } from './makeGroup';
 
 export { Home } from './home';
+
+export { NormalIcon } from './normalIcon';
+
+export { UpdateProfileImageIcon } from './updateProfileImageIcon';
+
+export { UpdateNameIcon } from './updateNameIcon';
