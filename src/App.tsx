2f9b58b98f5eb3a7dacdd6627891ac535c174740
jongjin <jongjin024@gmail.com>
2023-03-03 10:47:00 +0900

    add: create my page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import GlobalStyle from 'shared/style';
 import { Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
-import { FindPasswordPage, MainPage, SigninPage, SignupPage } from 'pages';
+import {
+  FindPasswordPage,
+  MainPage,
+  MyPage,
+  SigninPage,
+  SignupPage,
+} from 'pages';
 
 function App() {
   return (
@@ -12,6 +18,7 @@ function App() {
         <Route path='/auth/signin' element={<SigninPage />} />
         <Route path='/auth/signup' element={<SignupPage />} />
         <Route path='/auth/findPassword' element={<FindPasswordPage />} />
+        <Route path='/my' element={<MyPage />} />
       </Routes>
     </RecoilRoot>
   );
