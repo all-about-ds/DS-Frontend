7ef3fe6ba85a8b1e412731ce93462dffc6ba72ee
ParkSeoJune <tjwns4122@gmail.com>
2023-03-06 18:01:53 +0900

    add: react-toastify install

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from 'shared/style';
+import { ToastContainer } from 'react-toastify';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
+    <ToastContainer />
     <GlobalStyle />
     <App />
   </BrowserRouter>
