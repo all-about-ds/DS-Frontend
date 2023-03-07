286cd0fb3bd8702922d212e4815fd16f9dce4bd6
jongjin <jongjin024@gmail.com>
2023-03-07 16:09:30 +0900

    add: header 빼먹은 element

--- src/components/group/ui/groupPageHeader/index.tsx
+++ src/components/group/ui/groupPageHeader/index.tsx
@@ -4,10 +4,13 @@ import * as I from 'assets/svg';
 function GroupPageHeader() {
   return (
     <S.GroupPageHeader>
-      <S.Elements>
+      <S.Elements className='left'>
         <I.BackButton />
       </S.Elements>
-      <S.Elements>
+      <S.Elements className='center'>
+        <p>그룹정보</p>
+      </S.Elements>
+      <S.Elements className='right'>
         <I.ChattingIcon />
         <I.TimerIcon />
         <I.FaceTimeIcon />
--- src/components/group/ui/groupPageHeader/style.ts
+++ src/components/group/ui/groupPageHeader/style.ts
@@ -1,17 +1,26 @@
 import styled from '@emotion/styled';
 
 export const GroupPageHeader = styled.header`
+  position: relative;
   width: 100%;
   display: flex;
+  align-items: center;
   justify-content: space-between;
   margin: 2rem 0 1rem;
+  background-color: rgba(255, 255, 255, 0);
 `;
 
 export const Elements = styled.div`
   display: flex;
   gap: 16px;
+  color: white;
 
   & > svg {
     cursor: pointer;
+  }
+
+  &.center {
+    position: absolute;
+    left: 45%;
   }
 `;
