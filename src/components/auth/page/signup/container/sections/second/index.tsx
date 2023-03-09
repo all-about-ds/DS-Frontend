c66cf5e9ccdf58e5304a16fd26c60b9657753aef
jongjin <jongjin024@gmail.com>
2023-03-09 12:27:30 +0900

    fix: focus issue

--- src/components/auth/page/signup/container/sections/second/index.tsx
+++ src/components/auth/page/signup/container/sections/second/index.tsx
@@ -1,16 +1,23 @@
 import { timerAtom } from 'atoms';
 import AuthButton from 'components/auth/ui/button';
-import { KeyboardEventHandler, useEffect, useRef } from 'react';
+import { KeyboardEventHandler, useEffect, useRef, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import * as S from './style';
 
 function SignupSecondSection() {
   const [timer, setTimer] = useRecoilState(timerAtom);
+  const [inputs, setInputs] = useState(['', '', '', '']);
 
-  const input1 = useRef<HTMLInputElement>(null);
-  const input2 = useRef<HTMLInputElement>(null);
-  const input3 = useRef<HTMLInputElement>(null);
-  const input4 = useRef<HTMLInputElement>(null);
+  const handleChange = (index: number, value: string) => {
+    const newInputs = [...inputs];
+    newInputs[index] = value;
+    setInputs(newInputs);
+
+    if (value && index < 3) {
+      const nextInput = document.getElementById(`input${index + 2}`);
+      nextInput?.focus();
+    }
+  };
 
   useEffect(() => {
     const countdown = setInterval(() => {
@@ -34,60 +41,21 @@ function SignupSecondSection() {
     return () => clearInterval(countdown);
   }, [timer]);
 
-  const handleKeyDown: KeyboardEventHandler<HTMLInputElement> = (e) => {
-    if (e.key !== 'Backspace' && e.currentTarget.value.length === 1) {
-      switch (e.currentTarget.name) {
-        case 'input1':
-          input2.current?.focus();
-          break;
-        case 'input2':
-          input3.current?.focus();
-          break;
-        case 'input3':
-          input4.current?.focus();
-          break;
-        case 'input4':
-          input1.current?.focus();
-          break;
-        default:
-          break;
-      }
-    }
-  };
-
   return (
     <S.SecondSectionLayout>
       <S.Text>입력하신 이메일로 인증번호 4자리를 전송했어요</S.Text>
       <S.NumberForm>
-        <S.AuthenticationNumberInput
-          name={'input1'}
-          type='text'
-          maxLength={1}
-          ref={input1}
-          onKeyDown={handleKeyDown}
-          autoFocus
-        />
-        <S.AuthenticationNumberInput
-          name={'input2'}
-          type='text'
-          maxLength={1}
-          ref={input2}
-          onKeyDown={handleKeyDown}
-        />
-        <S.AuthenticationNumberInput
-          name={'input3'}
-          type='text'
-          maxLength={1}
-          ref={input3}
-          onKeyDown={handleKeyDown}
-        />
-        <S.AuthenticationNumberInput
-          name={'input4'}
-          type='text'
-          maxLength={1}
-          ref={input4}
-          onKeyDown={handleKeyDown}
-        />
+        {inputs.map((input, index) => (
+          <S.AuthenticationNumberInput
+            key={index}
+            id={`input${index + 1}`}
+            type='text'
+            value={input}
+            maxLength={1}
+            tabIndex={index + 1}
+            onChange={(e) => handleChange(index, e.target.value)}
+          />
+        ))}
       </S.NumberForm>
       <S.ResendBox>
         <p>인증번호가 오지않았나요?</p>
