cccf703b54c73c9650e2bc3ba94f3ab6910f71a6
jongjin <jongjin024@gmail.com>
2023-03-10 16:16:28 +0900

    add: 회원가입 기능구현

--- src/components/auth/page/signup/container/sections/third/index.tsx
+++ src/components/auth/page/signup/container/sections/third/index.tsx
@@ -1,23 +1,106 @@
 import AuthButton from 'components/auth/ui/button';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import * as S from './style';
+import * as I from 'assets/svg';
+import auth from 'api/auth';
+import { useRecoilState } from 'recoil';
+import { signupCurrentSectionAtom, signupEmailAtom } from 'atoms';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+interface UseFormType {
+  name: string;
+  password: string;
+}
 
 function SignupThirdSection() {
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [signupEmail, setSignupEmail] = useRecoilState(signupEmailAtom);
+  const [_, setSignupCurrentSection] = useRecoilState(signupCurrentSectionAtom);
+  const navigate = useNavigate();
+
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const toggleShowPassword = () => setShowPassword(!showPassword);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UseFormType>();
+
+  const onValid = async (data: UseFormType) => {
+    try {
+      await auth.signup({
+        name: data.name,
+        email: signupEmail,
+        password: data.password,
+      });
+      navigate('/auth/signin');
+      setSignupEmail('');
+      setSignupCurrentSection(1);
+      toast.success('회원가입 성공!');
+    } catch {
+      setErrorMessage('이미 있는 이름이에요');
+    }
+  };
+
+  const inValid = () => {
+    setErrorMessage('빈칸을 다시 확인해주세요');
+  };
+
   return (
-    <S.ThirdSectionLayout>
-      <S.Text>
-        이제 마지막 단계에요, DS에서 사용하실 닉네임과 비밀번호를 입력해주세요.
+    <S.ThirdSectionLayout onSubmit={handleSubmit(onValid, inValid)}>
+      <S.Text>사용하실 닉네임과 비밀번호를 입력해주세요.</S.Text>
+      <S.Text className='password-description'>
+        비밀번호는 8~16자 영문, 숫자, 특수문자를 사용하세요.
       </S.Text>
-      <S.InputWrapper>
-        <S.InputTitle>닉네임</S.InputTitle>
-        <S.Input placeholder='닉네임을 입력해주세요' type='text' />
+      <S.InputWrapper className='name'>
+        <S.InputTitle isError={Boolean(errorMessage)}>닉네임</S.InputTitle>
+        <S.Input
+          placeholder='닉네임을 입력해주세요'
+          type='text'
+          isError={Boolean(errorMessage)}
+          {...register('name', {
+            required: '이름은 필수 입력입니다.',
+          })}
+        />
       </S.InputWrapper>
-      <S.InputWrapper>
-        <S.InputTitle>비밀번호</S.InputTitle>
-        <S.Input placeholder='비밀번호를 입력해주세요' type='text' />
+      <S.InputWrapper className='password'>
+        <S.InputTitle isError={Boolean(errorMessage)}>비밀번호</S.InputTitle>
+        <S.InputBox>
+          <S.Input
+            className='password-input'
+            placeholder='비밀번호를 입력해주세요'
+            type={showPassword ? 'text' : 'password'}
+            isError={Boolean(errorMessage)}
+            {...register('password', {
+              required: '이름은 필수 입력입니다.',
+              minLength: {
+                message: '비밀번호는 8자 이상이어야해요.',
+                value: 8,
+              },
+              maxLength: {
+                message: '비밀번호는 최대 16자 입니다.',
+                value: 16,
+              },
+              pattern: {
+                message: '잘못된 비밀번호 형식이에요.',
+                value:
+                  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+              },
+            })}
+          />
+          <div onClick={toggleShowPassword}>
+            {!showPassword && <I.HidePassword />}
+            {showPassword && <I.ShowPassword />}
+          </div>
+        </S.InputBox>
       </S.InputWrapper>
-      <S.Div>
+      <S.ErrorText isError={errorMessage}>{errorMessage}</S.ErrorText>
+      <S.Box>
         <AuthButton>완료</AuthButton>
-      </S.Div>
+      </S.Box>
     </S.ThirdSectionLayout>
   );
 }
--- src/components/auth/page/signup/container/sections/third/style.ts
+++ src/components/auth/page/signup/container/sections/third/style.ts
@@ -1,43 +1,92 @@
 import styled from '@emotion/styled';
 
-export const ThirdSectionLayout = styled.section`
-  margin: 0.8rem auto -1rem;
+export const ThirdSectionLayout = styled.form`
+  margin: 0 auto -1rem;
   width: 18vw;
   min-width: 300px;
 `;
 
-export const Text = styled.p`
+export const Text = styled.h1`
   font-family: 'AppleSDGothicNeoM00';
   font-style: normal;
   font-weight: 400;
   font-size: 14px;
   color: #acacac;
-  text-align: left;
+  text-align: center;
   line-height: 20px;
-  margin-bottom: 2rem;
+  margin-bottom: 0.5rem;
+
+  &.password-description {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    text-align: center;
+    color: #acacac;
+  }
 `;
 
 export const InputWrapper = styled.div`
   width: 100%;
   min-width: 284px;
   margin: 0 auto 1.2rem;
+
+  &.name {
+    margin: 2rem auto 1.2rem;
+  }
+
+  &.password {
+    margin: 0 auto 2.5rem;
+  }
 `;
 
-export const InputTitle = styled.p`
+export const InputTitle = styled.p<{ isError: boolean | undefined }>`
   font-style: normal;
   font-weight: 400;
   font-size: 13px;
-  color: rgba(255, 255, 255, 0.9);
+  color: ${(e) => (e.isError ? '#EE3939' : 'rgba(255, 255, 255, 0.9)')};
   margin-left: 8px;
   margin: 0 0 10px 8px;
 `;
 
-export const Input = styled.input`
-  border: none;
+export const InputBox = styled.div`
+  display: flex;
+  background-color: #232323;
+  border-radius: 10px;
+  align-items: center;
+
+  @keyframes fadein {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+  @-webkit-keyframes fadein {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+  & > div > svg {
+    display: flex;
+    align-items: center;
+    margin-right: 15px;
+    cursor: pointer;
+    animation: fadein 0.3s;
+    -webkit-animation: fadein 0.3s;
+  }
+`;
+
+export const Input = styled.input<{ isError?: boolean | undefined }>`
+  border: ${(e) => (e.isError ? '1px solid #EE3939' : 'none')};
+  background: ${(e) => (e.isError ? '#412626' : '#232323')};
   width: 100%;
   height: 40px;
   border-radius: 10px;
-  background: #232323;
   font-style: normal;
   font-weight: 400;
   font-size: 14px;
@@ -52,7 +101,7 @@ export const Input = styled.input`
   }
 `;
 
-export const Div = styled.div`
+export const Box = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -60,6 +109,14 @@ export const Div = styled.div`
 
   button {
     width: 100%;
-    margin-top: 7.4rem;
+    margin-top: 5.9rem;
   }
+`;
+
+export const ErrorText = styled.p<{ isError: string }>`
+  display: ${(e) => (e.isError ? 'block' : 'none')};
+  color: #ee3939;
+  font-size: 13px;
+  text-align: center;
+  margin-bottom: ${(e) => (e.isError ? '-1rem' : '0')};
 `;
