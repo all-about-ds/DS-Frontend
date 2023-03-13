d6dd964b1ee1b882db2a95a89584b925fe427661
ParkSeoJune <tjwns4122@gmail.com>
2023-03-13 18:01:27 +0900

    add: member chattingBox ui

--- src/components/group/pages/chatting/index.tsx
+++ src/components/group/pages/chatting/index.tsx
@@ -1,5 +1,42 @@
+import GroupPageHeader from 'components/group/ui/groupPageHeader';
+import * as S from './style';
+import * as I from '../../../../assets/svg';
+
 function GroupChatting() {
-  return <></>;
+  return (
+    <>
+      <S.GroupChattingLayout>
+        <S.ChattingLayout>
+          <GroupPageHeader />
+          <S.ChattingWrapper>
+            <S.MemberWrapper>
+              <S.MemberBox>
+                <S.MemberProfile image='' />
+                <S.MemberName>오종진</S.MemberName>
+              </S.MemberBox>
+              <S.ChattingBox>
+                <S.Chatting>
+                  <S.ChattingText>
+                    애국가(愛國歌)는 ‘나라를 사랑하는 노래’라는 뜻이에요.
+                  </S.ChattingText>
+                </S.Chatting>
+                <S.Time>오후 1:34</S.Time>
+              </S.ChattingBox>
+            </S.MemberWrapper>
+          </S.ChattingWrapper>
+          <S.InputBox>
+            <S.InputInnerBox>
+              <I.LongRectengle />
+              <S.Input></S.Input>
+              <div style={{ cursor: 'pointer' }}>
+                <I.SubmitArrow />
+              </div>
+            </S.InputInnerBox>
+          </S.InputBox>
+        </S.ChattingLayout>
+      </S.GroupChattingLayout>
+    </>
+  );
 }
 
 export default GroupChatting;
