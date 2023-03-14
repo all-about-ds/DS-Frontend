fa71bf06311e0714bdaf3da44468ebd164adac3f
jongjin <jongjin024@gmail.com>
2023-03-14 09:28:53 +0900

    add: groupInformation api test

--- src/components/group/pages/information/index.tsx
+++ src/components/group/pages/information/index.tsx
@@ -5,6 +5,7 @@ import GroupPageHeader from 'components/group/ui/groupPageHeader';
 import { useParams } from 'react-router-dom';
 import group from 'api/group';
 import { GroupInformationInterface } from 'types/group.type';
+import { getGroup } from 'libs/getUrl';
 
 function GroupInformation() {
   const [isOwner, setIsOwner] = useState<boolean>(false);
@@ -17,9 +18,10 @@ function GroupInformation() {
         const res: any = await group.getGroupInformation(
           String(params.groupId)
         );
+        setIsOwner(res.data.host);
         setInformation(res.data);
       } catch (e: any) {
-        if (e.respones.status === 404) {
+        if (e.response.status === 404) {
           console.log('없는 그룹');
         }
       }
@@ -31,26 +33,12 @@ function GroupInformation() {
   return (
     <S.GroupInformationPageLayout>
       <GroupPageHeader />
-      <S.GroupImage
-        src='https://mblogthumb-phinf.pstatic.net/MjAxNzA0MTlfMTM1/MDAxNDkyNTQ0OTExODU2.O_zowy-4bYAqqjN0X-muG2w0Y5yCcS2ohYhSkiZwzC0g.GHDfrpt_xE8Wo_qhTOtfupeVs04Zb3DzHWT7BPF4Xccg.JPEG.badpark/1920X1080-06.jpg?type=w800'
-        alt='그룹 이미지'
-      />
+      <S.GroupImage src={information?.groupImg} alt='그룹 이미지' />
       <S.TitleBox>
-        <S.Title>그냥 공부하는 방</S.Title>
+        <S.Title>{information?.groupName}</S.Title>
         {isOwner && <I.OwnerButton />}
       </S.TitleBox>
-      <S.Description>
-        광주광역시 광산구 송정동에 위치한 SW 마이스터 고등학교로 대한민국에서 세
-        번째로 개교한 소프트웨어 마이스터고다. 광주광역시 광산구 송정동에 위치한
-        SW 마이스터 고등학교로 대한민국에서 세 번째로 개교한 소프트웨어
-        마이스터고다. 광주광역시 광산구 송정동에 위치한 SW 마이스터 고등학교로
-        대한민국에서 세 번째로 개교한 소프트웨어 마이스터고다. 광주광역시 광산구
-        송정동에 위치한 SW 마이스터 고등학교로 대한민국에서 세 번째로 개교한
-        소프트웨어 마이스터고다. 광주광역시 광산구 송정동에 위치한 SW 마이스터
-        고등학교로 대한민국에서 세 번째로 개교한 소프트웨어 마이스터고다.
-        광주광역시 광산구 송정동에 위치한 SW 마이스터 고등학교로 대한민국에서 세
-        번째로 개교한 소프트웨어 마이스터고다.
-      </S.Description>
+      <S.Description>{information?.groupDescription}</S.Description>
       <S.Line />
       <S.TextMembersBox>
         <S.TextMembers>그룹원들</S.TextMembers>
@@ -60,76 +48,25 @@ function GroupInformation() {
         <I.OwnerIcon />
       </div>
       <S.MemberList>
-        <S.MemberBox>
+        <S.MemberBox key={'head'}>
           <S.MemberImage
-            src='https://newsimg-hams.hankookilbo.com/2022/11/11/dba4d061-b5f9-486f-9bb8-cd09f5f75277.jpg'
+            src={information?.head.profileImg}
             alt='그룹원 이미지'
           />
           <div>
             <S.MemberRole>동아리 부장</S.MemberRole>
-            <S.MemberName>오종진님</S.MemberName>
+            <S.MemberName>{information?.head.name}</S.MemberName>
           </div>
         </S.MemberBox>
-        <S.MemberBox>
-          <S.MemberImage
-            src='https://newsimg-hams.hankookilbo.com/2022/11/11/dba4d061-b5f9-486f-9bb8-cd09f5f75277.jpg'
-            alt='그룹원 이미지'
-          />
-          <div>
-            <S.MemberRole>동아리 부장</S.MemberRole>
-            <S.MemberName>오종진님</S.MemberName>
-          </div>
-        </S.MemberBox>
-        <S.MemberBox>
-          <S.MemberImage
-            src='https://newsimg-hams.hankookilbo.com/2022/11/11/dba4d061-b5f9-486f-9bb8-cd09f5f75277.jpg'
-            alt='그룹원 이미지'
-          />
-          <div>
-            <S.MemberRole>동아리 부장</S.MemberRole>
-            <S.MemberName>오종진님</S.MemberName>
-          </div>
-        </S.MemberBox>
-        <S.MemberBox>
-          <S.MemberImage
-            src='https://newsimg-hams.hankookilbo.com/2022/11/11/dba4d061-b5f9-486f-9bb8-cd09f5f75277.jpg'
-            alt='그룹원 이미지'
-          />
-          <div>
-            <S.MemberRole>동아리 부장</S.MemberRole>
-            <S.MemberName>오종진님</S.MemberName>
-          </div>
-        </S.MemberBox>
-        <S.MemberBox>
-          <S.MemberImage
-            src='https://newsimg-hams.hankookilbo.com/2022/11/11/dba4d061-b5f9-486f-9bb8-cd09f5f75277.jpg'
-            alt='그룹원 이미지'
-          />
-          <div>
-            <S.MemberRole>동아리 부장</S.MemberRole>
-            <S.MemberName>오종진</S.MemberName>
-          </div>
-        </S.MemberBox>
-        <S.MemberBox>
-          <S.MemberImage
-            src='https://newsimg-hams.hankookilbo.com/2022/11/11/dba4d061-b5f9-486f-9bb8-cd09f5f75277.jpg'
-            alt='그룹원 이미지'
-          />
-          <div>
-            <S.MemberRole>동아리 부장</S.MemberRole>
-            <S.MemberName>오종진님</S.MemberName>
-          </div>
-        </S.MemberBox>
-        <S.MemberBox>
-          <S.MemberImage
-            src='https://newsimg-hams.hankookilbo.com/2022/11/11/dba4d061-b5f9-486f-9bb8-cd09f5f75277.jpg'
-            alt='그룹원 이미지'
-          />
-          <div>
-            <S.MemberRole>동아리 부장</S.MemberRole>
-            <S.MemberName>오종진님</S.MemberName>
-          </div>
-        </S.MemberBox>
+        {information?.memberList.map((member) => (
+          <S.MemberBox key={member.idx}>
+            <S.MemberImage src={member.profileImg} alt='그룹원 이미지' />
+            <div>
+              <S.MemberRole>동아리원</S.MemberRole>
+              <S.MemberName>{member.name}</S.MemberName>
+            </div>
+          </S.MemberBox>
+        ))}
       </S.MemberList>
       {isOwner && <S.RemoveGroupButton>그룹삭제</S.RemoveGroupButton>}
     </S.GroupInformationPageLayout>
