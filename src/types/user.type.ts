467c280d8ae3330f4806c44646b8be01193db828
jongjin <jongjin024@gmail.com>
2023-03-21 12:25:46 +0900

    fix: GetMyInfoInterface interface

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,10 +1,12 @@
 export interface GetMyInfoInterface {
+  idx: number;
   name: string;
   img: string;
   myGroupList: MyGroupListInterface[];
 }
 
 export interface MyGroupListInterface {
+  idx: number;
   name: string;
   img: string;
 }
