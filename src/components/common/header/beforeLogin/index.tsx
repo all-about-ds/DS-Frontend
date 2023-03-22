2219ed26280edacc9355532a9371759dfb6666a5
jongjin <jongjin024@gmail.com>
2023-03-22 20:31:21 +0900

    add: BeforeLoginHeader Link

--- src/components/common/header/beforeLogin/index.tsx
+++ src/components/common/header/beforeLogin/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import * as S from './style';
 
 function BeforeLoginHeader() {
   return (
     <S.HeaderContentBox>
-      <S.Text>로그인</S.Text>
-      <S.Text>회원가입</S.Text>
+      <Link to={'/auth/signin'}>
+        <S.Text>로그인</S.Text>
+      </Link>
+      <Link to={'/auth/signup '}>
+        <S.Text>회원가입</S.Text>
+      </Link>
     </S.HeaderContentBox>
   );
 }
