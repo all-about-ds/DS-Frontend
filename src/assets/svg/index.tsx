d162d70e4e9102e1112d956d3fb5bafeed02feae
jongjin <jongjin024@gmail.com>
2023-03-22 19:42:00 +0900

    add: MainLogo

--- src/assets/svg/index.tsx
+++ src/assets/svg/index.tsx
@@ -1,45 +1,24 @@
 export { Search, GraySearch } from './search';
-
 export { MakeGroup } from './makeGroup';
-
 export { Home } from './home';
-
 export { NormalIcon } from './normalIcon';
-
 export { UpdateProfileImageIcon } from './updateProfileImageIcon';
-
 export { UpdateNameIcon } from './updateNameIcon';
-
 export { Photo } from './photo';
-
 export { ArrowUp, ArrowDown, SubmitArrow } from './arrow';
-
 export { ThreeDot, PurpleDot } from './threeDot';
-
 export { Lock } from './lock';
-
 export { Exit } from './exit';
-
 export { AuthGoBackIcon } from './authGoBackIcon';
-
 export { AuthExitIcon } from './authExitIcon';
-
 export { BackButton } from './backButton';
-
 export { FaceTimeIcon } from './faceTimeIcon';
-
 export { InformationIcon } from './informationIcon';
-
 export { ChattingIcon } from './chattingIcon';
-
 export { TimerIcon } from './timerIcon';
-
 export { OwnerButton, DeleteButton } from './ownerButton';
-
 export { OwnerIcon } from './ownerIcon';
-
 export { LongRectengle } from './rectengle';
-
 export { ShowPassword } from './showPassword';
-
 export { HidePassword } from './hidePassword';
+export { Logo } from './logo';
