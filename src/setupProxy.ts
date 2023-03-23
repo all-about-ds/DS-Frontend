34ad3bfd088fb4c7d94edebb1585215aa3cc9890
ParkSeoJune <tjwns4122@gmail.com>
2023-03-23 23:17:49 +0900

    fix: setupProxy error

--- src/setupProxy.ts
+++ src/setupProxy.ts
@@ -1,6 +1,6 @@
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
-module.exports = (app: any) => {
+export default (app: any) => {
   app.use(
     '/ws',
     createProxyMiddleware({ target: 'http://localhost:3000', ws: true })
