9838766b4e392eb61362f2904a04730d9f4277cb
ParkSeoJune <tjwns4122@gmail.com>
2023-03-29 09:18:09 +0900

    fix: defaultProfile svg size

--- src/assets/svg/defaultProfile.tsx
+++ src/assets/svg/defaultProfile.tsx
@@ -4,7 +4,7 @@ export const DefaultProfile = () => {
       <svg
         width='36'
         height='36'
-        viewBox='0 0 36 36'
+        viewBox='0 0 120 120'
         fill='none'
         xmlns='http://www.w3.org/2000/svg'
       >
