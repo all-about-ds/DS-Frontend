80412367dc6bd9c0bfeff4022189eaa000d115aa
ParkSeoJune <tjwns4122@gmail.com>
2023-05-30 13:44:56 +0900

    add: chat type

--- src/types/chat.type.ts
+++ src/types/chat.type.ts
@@ -1,7 +1,17 @@
-export type ChatType = {
+export interface ChatDataType {
+  [key: number]: {
+    img: string;
+    name: string;
+    chat: string;
+    createdAt: string;
+    isMine: boolean;
+  };
+}
+
+export interface ChatMessageType {
   img: string;
   name: string;
   chat: string;
-  createdAt: string;
+  createAt: string;
   isMine: boolean;
-};
+}
