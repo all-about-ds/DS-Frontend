8d20f7c73ea433bd20e5796094810eaa038deaba
Jongjin <jongjin024@gmail.com>
2023-06-01 14:37:32 +0900

    fix: header width에 의해 생성되는 하단 스크롤바 제거

--- src/components/common/header/style.ts
+++ src/components/common/header/style.ts
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 
 export const HeaderLayout = styled.header`
-  width: 100vw;
+  width: 100%;
   height: 80px;
   background: rgba(16, 16, 16, 0.56);
   z-index: 999;
@@ -12,19 +12,19 @@ export const HeaderLayout = styled.header`
 
 export const HeaderBottomBar = styled.div`
   background: rgba(255, 255, 255, 0.17);
-  width: 100vw;
+  width: 100%;
   height: 1px;
   z-index: 999;
 `;
 
 export const HeaderContentWrapper = styled.div`
-  width: 66.25vw;
+  width: 66.25%;
   display: flex;
   justify-content: space-between;
   align-items: center;
 
   @media screen and (max-width: 1050px) {
-    width: 97.5vw;
+    width: 97.5%;
   }
 `;
 
