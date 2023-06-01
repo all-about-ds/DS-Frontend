a27b71cae31f06dbd7939095b405b7852af68f18
Jongjin <jongjin024@gmail.com>
2023-06-01 14:39:48 +0900

    fix: 하단 스크롤바 생성 이슈에 의한 width 단위 변경

--- src/components/common/layout/align/center/style.ts
+++ src/components/common/layout/align/center/style.ts
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 
 export const CenterAlignmentLayout = styled.div`
-  width: 100vw;
+  width: 100%;
   height: calc(100vh - 81px);
   display: flex;
   flex-direction: column;
