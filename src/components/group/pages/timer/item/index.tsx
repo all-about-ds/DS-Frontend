7ecc4f422c4304ae190cfa88e35fae728ca7513b
jongjin1010 <jongjin024@gmail.com>
2023-06-04 00:52:15 +0900

    add: timer 기능 구현

--- src/components/group/pages/timer/item/index.tsx
+++ src/components/group/pages/timer/item/index.tsx
@@ -1,13 +1,35 @@
 import * as S from './style';
 import * as I from 'assets/svg';
 
-function MemberTimerItem({ isActive }: { isActive: boolean }) {
+interface MemberTimerItemProps {
+  memberName: string;
+  memberTime: number;
+  isActive: boolean;
+}
+
+function MemberTimerItem(props: MemberTimerItemProps) {
+  const format = (time: number) => {
+    const hour =
+      time / 3600 > 9
+        ? parseInt(String(time / 3600))
+        : '0' + parseInt(String(time / 3600));
+
+    const min =
+      (time % 3600) / 60 > 9
+        ? parseInt(String((time % 3600) / 60))
+        : '0' + parseInt(String(time / 3600));
+
+    const sec = time % 60 > 9 ? time % 60 : '0' + (time % 60);
+
+    return hour + ':' + min + ':' + sec;
+  };
+
   return (
-    <S.MemberTimerItem isActive={isActive}>
+    <S.MemberTimerItem isActive={props.isActive}>
       <I.TimerIcon />
       <div>
-        <S.MemberName>진시윤님</S.MemberName>
-        <S.MemberTimer>00:00:00</S.MemberTimer>
+        <S.MemberName>{props.memberName}</S.MemberName>
+        <S.MemberTimer>{format(props.memberTime)}</S.MemberTimer>
       </div>
     </S.MemberTimerItem>
   );
--- src/components/group/pages/timer/page/index.tsx
+++ src/components/group/pages/timer/page/index.tsx
@@ -1,39 +1,129 @@
 import GroupPageHeader from 'components/group/ui/groupPageHeader';
+import { Unsubscribe, off, onValue, ref } from '@firebase/database';
+import { db } from '../../../../../firebase';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { TimerUserInterface } from 'types/user.type';
 import MemberTimerItem from '../item';
 import * as S from './style';
+import { useRecoilState } from 'recoil';
+import { userInfoAtomFamily } from 'atoms/container';
+import { set } from 'firebase/database';
+
+type MyInfo = TimerUserInterface;
 
 function GroupTimer() {
+  const [users, setUsers] = useState<TimerUserInterface[]>([]);
+  const [active, setActive] = useState<boolean>(false);
+  const [name] = useRecoilState(userInfoAtomFamily('name'));
+
+  const location = useLocation();
+
+  const [myInfo, setMyInfo] = useState<MyInfo>({
+    name: '',
+    time: 0,
+    active: false,
+  });
+
+  useEffect(() => {
+    const timerRef = ref(db, `timers/${location.state.groupName}/users`);
+
+    const unscribe: Unsubscribe = onValue(timerRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        const timer: TimerUserInterface[] = Object.values(data);
+        setUsers(timer);
+        timer.forEach((item) => {
+          if (item.name === name) {
+            setMyInfo(item);
+          }
+        });
+      }
+    });
+
+    return () => {
+      off(timerRef, 'value', unscribe);
+    };
+  }, []);
+
+  useEffect(() => {
+    let time = Number(myInfo?.time);
+
+    if (active) {
+      const id = setInterval(() => {
+        time = time += 1;
+        set(ref(db, `timers/${location.state.groupName}/users/${name}`), {
+          name: name,
+          time: time,
+          active: active,
+        });
+      }, 1000);
+
+      return () => clearInterval(id);
+    } else {
+      set(ref(db, `timers/${location.state.groupName}/users/${name}`), {
+        name: name,
+        time: myInfo?.time,
+        active: active,
+      });
+    }
+  }, [active]);
+
+  const format = (type: string) => {
+    const time = myInfo.time;
+
+    if (type == 'hour') {
+      return time / 3600 > 9
+        ? parseInt(String(time / 3600))
+        : '0' + parseInt(String(time / 3600));
+    }
+
+    if (type == 'minute') {
+      return (time % 3600) / 60 > 9
+        ? parseInt(String((time % 3600) / 60))
+        : '0' + parseInt(String(time / 3600));
+    }
+
+    if (type == 'second') {
+      return time % 60 > 9 ? time % 60 : '0' + (time % 60);
+    }
+  };
+
   return (
     <S.GroupTimerPageLayout>
       <GroupPageHeader title={'타이머'} />
       <S.MyTimerBox>
         <div className='hour'>
           <S.ElementType>시간</S.ElementType>
-          <S.ElementValue>00</S.ElementValue>
+          <S.ElementValue>{format('hour')}</S.ElementValue>
         </div>
         <S.Colon>:</S.Colon>
         <div className='minute'>
           <S.ElementType>분</S.ElementType>
-          <S.ElementValue>00</S.ElementValue>
+          <S.ElementValue>{format('minute')}</S.ElementValue>
         </div>
         <S.Colon>:</S.Colon>
         <div className='second'>
           <S.ElementType>초</S.ElementType>
-          <S.ElementValue>00</S.ElementValue>
+          <S.ElementValue>{format('second')}</S.ElementValue>
         </div>
       </S.MyTimerBox>
       <S.ButtonDecorate />
-      <S.TimerButton>시작</S.TimerButton>
+      {!active && (
+        <S.TimerButton onClick={() => setActive(true)}>시작</S.TimerButton>
+      )}
+      {active && (
+        <S.TimerButton onClick={() => setActive(false)}>중단</S.TimerButton>
+      )}
       <S.MemberTimerBox>
-        <MemberTimerItem isActive={true} />
-        <MemberTimerItem isActive={false} />
-        <MemberTimerItem isActive={false} />
-        <MemberTimerItem isActive={false} />
-        <MemberTimerItem isActive={false} />
-        <MemberTimerItem isActive={false} />
-        <MemberTimerItem isActive={false} />
-        <MemberTimerItem isActive={false} />
-        <MemberTimerItem isActive={false} />
+        {users.map((item, index) => (
+          <MemberTimerItem
+            key={index}
+            memberName={item.name}
+            memberTime={item.time}
+            isActive={item.active}
+          />
+        ))}
       </S.MemberTimerBox>
     </S.GroupTimerPageLayout>
   );
