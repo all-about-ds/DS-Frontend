511d33eebb36c836bb4d24263d782dfb721d46f4
ParkSeoJune <tjwns4122@gmail.com>
2023-06-08 18:07:27 +0900

    remove: useless data

--- src/components/group/ui/manageGroup/index.tsx
+++ src/components/group/ui/manageGroup/index.tsx
@@ -63,10 +63,6 @@ function ManageGroup({ groupType }: { groupType: ManageGroupType }) {
 
         if (groupType === 'create') {
           await group.createGroup(req);
-          set(ref(db, `chattings/${data.name}/users/` + location.state.name), {
-            name: location.state.name,
-            profile: location.state.profile,
-          });
           set(ref(db, `timers/${data.name}/users/${userName}`), {
             name: userName,
             time: 0,
